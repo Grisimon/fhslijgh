9d69587ce51a2d9a6bc5f76939ecf658cf1b7198
Grisimon <simon.carles@elev.ga.lbs.se>
2024-05-22 11:42:16 +0200

    IM LITERALLY GOING FUCKING INSANE THERE ARE WORMES UNDER MY SKIN I NEED TO DIG THEM OUT WITH A SCREWDRIVER
    HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG

---
+++ sound popup test/popup.js
@@ -0,0 +1,18 @@
+// script.js
+document.addEventListener('DOMContentLoaded', (event) => {
+    var audio = document.getElementById('backgroundMusic');
+    var popup = document.getElementById('popup');
+    var button = document.getElementById('playButton');
+
+    // Show the popup
+    popup.style.display = 'block';
+
+    button.addEventListener('click', function() {
+        audio.play().then(() => {
+            console.log('Music is playing');
+            popup.style.display = 'none'; // Hide the popup after playing music
+        }).catch((error) => {
+            console.log('Failed to play music:', error);
+        });
+    });
+});
