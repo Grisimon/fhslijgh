9671aa536060c379591ed9c5fac095c77aea9120
Grisimon <simon.carles@elev.ga.lbs.se>
2024-09-03 14:24:30 +0200

    WHALLAH SHURDA JAG E TILLBAKA
    jag kommer att döda en familj av fem och en av dem kommer att vara skaparen till css. HÖR MIN VARNING DU KOMMER ATT DÖ

---
+++ shitter.php
@@ -0,0 +1,8 @@
+<?php
+header("content-type: text/html; charset=utf-8");
+if (empty($_GET["name"])) {
+    echo "<h1>hello world <h1>";
+} else { 
+    echo '<h1>hej', filter_input('input_get', 'name', FILTER_SANITIZE_SPECIAL_CHARS()),'<h1>';
+    
+}
